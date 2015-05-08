3a85d199415ec40b61f75a3a02affdc844fc43ef
Alexandre Baron <baron.alx@gmail.com>
2015-05-08 15:47:35 +0200

    Modify TitleScreen to use a random background + binary textures

---
+++ Assets/Scripts/BackgroundPicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundPicker : MonoBehaviour {
+
+	public Sprite[] backgrounds;
+
+	// Use this for initialization
+	void Start() {
+		PickRandom();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PickRandom() {
+		// Select a random background (we SHOULD have at least 1 background !)
+		int backgroundIdx = Random.Range(0, backgrounds.Length);
+		print ("Selectiong background " + backgroundIdx);
+		SpriteRenderer rndr = GetComponent<SpriteRenderer>();
+		rndr.sprite = backgrounds[backgroundIdx];
+	}
+}
