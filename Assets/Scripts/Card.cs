f3fc9788e346eb14eefd18efdd8913ddbf682397
etbh <chimyx@live.fr>
2015-05-09 16:44:40 +0200

    Card system

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	public bool isSelected = false;
+	public Figure figure;
+
+	public void OnMouseDown(){
+		isSelected = true;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,35 @@ public class Player : MonoBehaviour {
 
 	void Start() {
 		hasPlayed = true;
+		selecting = false;
 	}
 	void Update () {
-		if (!hasPlayed){
-			string text = "main :\n";
-			foreach (var finger in Hand)
-				text += finger.ToString() + ", ";
-			text += "\nfigures possibles :\n";
-			foreach (var figure in formableFigures())
-				text += figure.Name + ", ";
-			gameObject.GetComponent<GUIText>().text = text;
-			//hasPlayed = true;
+		if (selecting){
+			IEnumerable<GameObject> selected = cards.Where(card => card.GetComponent<Card>().isSelected);
+			if (selected.Count() == 1){
+				Figure = selected.First().GetComponent<Card>().figure;
+				hasPlayed = true;
+				selecting = false;
+				cards.ForEach(Destroy);
+			}
+		}
+		else if (!hasPlayed){
+			selecting = true;
+			Debug.Log(Hand.Count()+" digits lefts");
+			cards = new List<GameObject>();
+			var formable = formableFigures();
+			for (int i=0; i<formable.Count(); ++i){
+				var figure = formable.ElementAt(i);
+				var card = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Card"));
+				card.transform.parent = gameObject.transform;			
+				card.GetComponentsInChildren<SpriteRenderer>()[1].sprite = Resources.Load<Sprite>("Figures/"+figure.Name);
+				card.GetComponentsInChildren<SpriteRenderer>()[2].sprite = Resources.Load<Sprite>("Figures/"+figure.Name+"_name");
+				cards.Add(card);
+				card.GetComponent<Card>().figure = figure;
+				float totalrange = formable.Count() * card.GetComponent<SpriteRenderer>().bounds.size.x * 1.1f;
+				card.transform.localPosition = new Vector3((i - formable.Count()/2) * totalrange / formable.Count(),0,0);
+
+			}
 		}
 
 	}
@@ -33,6 +51,8 @@ public class Player : MonoBehaviour {
 
 	public Figure Figure;
 	private bool hasPlayed;
+	private bool selecting;
+	private List<GameObject> cards;
 
 	public IEnumerator WaitForPlay(){
 		hasPlayed = false;
