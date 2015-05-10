77305ad192220cf0cdfd028c9971323a23a8117d
Alexandre Baron <baron.alx@gmail.com>
2015-05-10 11:47:23 +0200

    Fix description card movement

--- Assets/Scripts/DetailsCard.cs
+++ Assets/Scripts/DetailsCard.cs
@@ -4,14 +4,39 @@ using System.Collections;
 public class DetailsCard : MonoBehaviour {
 
 	private static DetailsCard instance;
-	public static DetailsCard get() { return instance;}
+	public static DetailsCard get() { return instance; }
+	public GameObject cameraObject;
 
 	private SpriteRenderer[] renderers;
+	private SpriteRenderer myRenderer;
+	private Camera myCam;
 
 	public void Start(){
 		renderers = gameObject.GetComponentsInChildren<SpriteRenderer>();
 		instance = this;
 		gameObject.SetActive(false);
+		myCam = cameraObject.GetComponent<Camera>();
+		myRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void Update() {
+		// Follow the mouse position if the object is active (i.e. if the mouse is on a figure card).
+		if (this.isActiveAndEnabled) {
+			// Mouse pos is in screen coords. Convert it to position the desc card in the world
+			Vector3 mouseWorldPos = myCam.ScreenToWorldPoint(Input.mousePosition);
+			// Put the desc card at upper left position relatively to mouse
+			Vector3 posOffset = new Vector3(-2.0f, -2.5f, 0.0f);
+			Vector3 newPos = mouseWorldPos - posOffset;
+
+			// Ensure Z is 0 to be visible
+			newPos.z = 0.0f;
+			// Ensure desc card center is at most at half its width before right border
+			Vector3 screenWidth = new Vector3(Screen.width, 0.0f, 0.0f);
+			Vector3 screenRightBorderWorldPos = myCam.ScreenToWorldPoint(screenWidth);
+			if (newPos.x <= (screenRightBorderWorldPos.x - (myRenderer.sprite.bounds.max.x / 2))) {
+				transform.position = newPos;
+			}
+		}
 	}
 
 	public void resetFigure(){
