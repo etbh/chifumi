fa21f1d0dace6018f537bccaef90140d0bbdc912
etbh <chimyx@live.fr>
2015-05-10 10:14:09 +0200

    Detail cards now is shown only on card hover

--- Assets/Scripts/DetailsCard.cs
+++ Assets/Scripts/DetailsCard.cs
@@ -11,16 +11,19 @@ public class DetailsCard : MonoBehaviour {
 	public void Start(){
 		renderers = gameObject.GetComponentsInChildren<SpriteRenderer>();
 		instance = this;
+		gameObject.SetActive(false);
 	}
 
 	public void resetFigure(){
 		renderers[1].sprite = null;
 		renderers[2].sprite = null;
+		gameObject.SetActive(false);
 	}
 
 	public void changeFigure(Figure figure){
 		renderers[1].sprite = Resources.Load<Sprite>("Figures/"+figure.Name);
 		renderers[2].sprite = Resources.Load<Sprite>("Figures/"+figure.Name+"_name");
+		gameObject.SetActive(true);
 	}
 
 }
