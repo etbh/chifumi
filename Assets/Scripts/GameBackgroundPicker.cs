5734ed9b52b2898aa3a846ff2d5b14b1f94f13a6
Alexandre Baron <baron.alx@gmail.com>
2015-05-10 12:38:36 +0200

    Forgot game background picker script

---
+++ Assets/Scripts/GameBackgroundPicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GameBackgroundPicker : MonoBehaviour {
+
+	public Sprite[] backgrounds;
+
+	// Use this for initialization
+	void Start() {
+		PickRandom();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PickRandom() {
+		// Select a random background (we SHOULD have at least 1 background !)
+		int backgroundIdx = Random.Range(0, backgrounds.Length);
+		SpriteRenderer myRenderer = GetComponent<SpriteRenderer>();
+		myRenderer.sprite = backgrounds[backgroundIdx];
+	}
+}
